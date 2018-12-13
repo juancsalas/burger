adf8ebec7d68ed7d752ab3b1b39ab57f381b39db
Juan Carlos Salas <JuanCarlosSalas@Katrinas-MacBook-Air.local>
2018-12-12 22:03:46 -0800

    adjusted server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require("express");
 
+var app = express();
 var PORT = process.env.PORT || 8080;
 
-var app = express();
 
 app.use(express.static("public"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
