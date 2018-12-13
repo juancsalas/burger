e1a28ec7aff37d79fbb7c15b6be34730752cfd33
Juan Carlos Salas <JuanCarlosSalas@Katrinas-MacBook-Air.local>
2018-12-12 22:31:32 -0800

    fixing heroku

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-
 app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
